58dacf735d2a62b95185c71a859e4fbbe065dc4d
Jessica Wilson <jessica.wilson@teamcolab.com>
2020-04-28 17:42:53 -0400

    Wp and plugin updates

--- wp-content/themes/toro-foods/webpack.mix.js
+++ wp-content/themes/toro-foods/webpack.mix.js
@@ -12,8 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix
-	.js('src/scripts/app.js', 'dist/')
-	.sass('src/styles/app.scss', 'dist/')
+	.js('src/scripts/app.js', 'dist/scripts')
+	.sass('src/styles/app.scss', 'dist/styles')
 	.copyDirectory('src/images', 'dist/images')
 	.browserSync({
 		host: 'toro-foods.lndo.site',
